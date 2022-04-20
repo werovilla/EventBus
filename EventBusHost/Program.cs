4697e641bc9821edc63b29b9d5bd7fb204b036fb
Hector Villa <hector.roman@mhsglobal.com>
2022-04-20 15:24:36 -0600

    3

--- EventBusHost/Program.cs
+++ EventBusHost/Program.cs
@@ -1,9 +1,14 @@
+using System.Reflection;
 using Autofac;
+using Autofac.Core.Lifetime;
+using Autofac.Extensions.DependencyInjection;
 using EventBus.EventBus;
 using EventBus.EventBus.Abstractions;
+using EventBus.EventBus.Events;
 using EventBus.EventBusRabbitMQ;
 using EventBus.EventBusServiceBus;
 using Producer.Controllers;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +19,46 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+if (builder.Configuration.GetValue<bool>("AzureServiceBusEnabled"))
+{
+    builder.Services.AddSingleton<IServiceBusPersisterConnection>(sp =>
+    {
+        var serviceBusConnectionString = builder.Configuration["EventBusConnection"];
+        var subscriptionClientName = builder.Configuration["SubscriptionClientName"];
+
+        return new DefaultServiceBusPersisterConnection(serviceBusConnectionString);
+    });
+}
+else
+{
+    builder.Services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+    {
+        var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+        var factory = new ConnectionFactory()
+        {
+            HostName = builder.Configuration["EventBusConnection"],
+            DispatchConsumersAsync = true
+        };
+
+        if (!string.IsNullOrEmpty(builder.Configuration["EventBusUserName"]))
+        {
+            factory.UserName = builder.Configuration["EventBusUserName"];
+        }
+
+        if (!string.IsNullOrEmpty(builder.Configuration["EventBusPassword"]))
+        {
+            factory.Password = builder.Configuration["EventBusPassword"];
+        }
+
+        var retryCount = 5;
+        if (!string.IsNullOrEmpty(builder.Configuration["EventBusRetryCount"]))
+        {
+            retryCount = int.Parse(builder.Configuration["EventBusRetryCount"]);
+        }
+
+        return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
+    });
+}
 
 if (builder.Configuration.GetValue<bool>("AzureServiceBusEnabled"))
 {
@@ -28,7 +73,6 @@ if (builder.Configuration.GetValue<bool>("AzureServiceBusEnabled"))
         return new EventBusServiceBus(serviceBusPersisterConnection, logger,
             eventBusSubcriptionsManager, iLifetimeScope, subscriptionName);
     });
-
 }
 else
 {
@@ -53,8 +97,9 @@ else
 builder.Services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
 builder.Services.AddSingleton<IPublishEventController, PublishEventController>();
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-var app = builder.Build();
+var app =  builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
